9f9f78aa67d67fcd2564a2fd0c1a3ab86ac2f75f
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2022-09-30 00:54:32 +0300

    modify Module contract

--- contracts/gnosisWallet/Module.sol
+++ contracts/gnosisWallet/Module.sol
@@ -1,14 +1,17 @@
 //SPDX-License-Identifier: GPL
 pragma solidity ^0.8.12;
 import "./MasterCopy.sol";
-import "./EIP4337Manager.sol";
+import "@safe-global/safe-contracts/contracts/base/ModuleManager.sol";
+
 
 /// @title Module - Base class for modules.
 /// @author Stefan George - <stefan@gnosis.pm>
 /// @author Richard Meissner - <richard@gnosis.pm>
+/// @author modified by CandideWallet Team
+
 contract Module is MasterCopy {
 
-    EIP4337Manager public manager;
+    ModuleManager public manager;
 
     modifier authorized() {
         require(msg.sender == address(manager), "Method can only be called from manager");
@@ -21,6 +24,6 @@ contract Module is MasterCopy {
         // manager can only be 0 at initalization of contract.
         // Check ensures that setup function can only be called once.
         require(address(manager) == address(0), "Manager has already been set");
-        manager = EIP4337Manager(payable(msg.sender));
+        manager = ModuleManager(msg.sender);
     }
 }
