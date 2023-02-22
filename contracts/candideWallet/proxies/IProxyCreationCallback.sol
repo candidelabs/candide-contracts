cd02756b3f472c33ab60d8725e594c5c714ffd3c
Andrew Wahid <andrew.wahid1@gmail.com>
2023-02-22 21:40:25 +0200

    feat: add proxy factory

---
+++ contracts/candideWallet/proxies/IProxyCreationCallback.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.7.0 <0.9.0;
+import "./CandideWalletProxy.sol";
+
+interface IProxyCreationCallback {
+    function proxyCreated(CandideWalletProxy proxy, address _singleton, bytes calldata initializer, uint256 saltNonce) external;
+}
