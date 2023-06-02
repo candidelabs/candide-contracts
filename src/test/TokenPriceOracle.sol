52ac3024bf0ebbf189cb7a2dbf8e19064cd8ad4f
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2023-06-02 07:09:58 +0300

    change state mutability to pure and remove unused variable

--- src/test/TokenPriceOracle.sol
+++ src/test/TokenPriceOracle.sol
@@ -10,7 +10,7 @@ contract TokenPriceOracle is IOracle{
     /**
      * return amount of tokens that are required to receive that much eth.
      */
-    function getTokenValueOfEth(uint256 ethOutput)  external view returns (uint256 tokenInput){
+    function getTokenValueOfEth(uint256) external pure returns (uint256 tokenInput){
 			return 1;
 	}
 }
